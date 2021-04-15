7d962981d21a24bd3b9733bbb3400baf569e053d
öykü <oyku_1224@hotmail.com>
2021-04-15 11:31:55 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebasenpm install react-bootstrap bootstrap'
+var firebaseConfig = {
+    apiKey: "AIzaSyCejzuw8Xn0ODcNKbO4-PTXeQ94bUhwFDk",
+    authDomain: "react-realtime-app-4d49b.firebaseapp.com",
+    databaseURL: "https://react-realtime-app-4d49b-default-rtdb.firebaseio.com",
+    projectId: "react-realtime-app-4d49b",
+    storageBucket: "react-realtime-app-4d49b.appspot.com",
+    messagingSenderId: "437530178519",
+    appId: "1:437530178519:web:14d3ea250d9cc3f5279192"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
